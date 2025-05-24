0db6f9b16b91d298d7876bbe34f042f8333340bc
Jayco Bea <jcobea.dev@gmail.com>
2025-05-24 17:50:29 +0800

    Update app drawer, add animation

---
+++ lib/components/app_drawer.dart
@@ -0,0 +1,98 @@
+import 'package:flutter/material.dart';
+import 'package:pasacao_hotline/components/feedback.dart';
+import 'package:pasacao_hotline/pages/about_page.dart';
+import 'package:package_info_plus/package_info_plus.dart';
+import 'package:lottie/lottie.dart';
+
+class AppDrawer extends StatefulWidget {
+  const AppDrawer({super.key});
+
+  @override
+  State<AppDrawer> createState() => _AppDrawerState();
+}
+
+class _AppDrawerState extends State<AppDrawer> {
+  String _version = '';
+
+  @override
+  void initState() {
+    super.initState();
+    _loadVersion();
+  }
+
+  Future<void> _loadVersion() async {
+    final info = await PackageInfo.fromPlatform();
+    setState(() {
+      _version = 'Version ${info.version}+${info.buildNumber}';
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      backgroundColor: Colors.grey[100],
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Column(
+            children: [
+              // ICON ANIMATION
+              Container(
+                padding: const EdgeInsets.only(top: 80),
+                alignment: Alignment.center,
+                child: Lottie.asset('assets/animations/phone.json', width: 150),
+              ),
+
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                child: Divider(
+                  color: Colors.grey[800],
+                ),
+              ),
+
+              // ABOUT
+              Padding(
+                padding: const EdgeInsets.only(left: 25.0),
+                child: ListTile(
+                  leading: const Icon(Icons.info),
+                  title: const Text('A B O U T'),
+                  onTap: () {
+                    Navigator.pop(context);
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (_) => const AboutPage()),
+                    );
+                  },
+                ),
+              ),
+
+              // FEEDBACK
+              Padding(
+                padding: const EdgeInsets.only(left: 25),
+                child: ListTile(
+                  leading: const Icon(Icons.feedback),
+                  title: const Text('F E E D B A C K'),
+                  onTap: () {
+                    Navigator.pop(context);
+                    showFeedbackDialog(context);
+                  },
+                ),
+              ),
+            ],
+          ),
+
+          // VERSION + BUILD NUMBER
+          Padding(
+            padding: const EdgeInsets.only(left: 25.0, bottom: 25.0),
+            child: ListTile(
+              leading: Text(
+                _version,
+                style: const TextStyle(fontSize: 14),
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,12 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:pasacao_hotline/components/feedback.dart';
+import 'package:pasacao_hotline/components/app_drawer.dart';
 import 'package:pasacao_hotline/components/office_tile.dart';
 import 'package:pasacao_hotline/models/office.dart';
-import 'package:pasacao_hotline/pages/about_page.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'package:pasacao_hotline/pages/office_details_page.dart';
-import 'package:package_info_plus/package_info_plus.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -17,14 +15,12 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   List<Office> offices = [];
-  String _version = '';
 
   @override
   void initState() {
     super.initState();
     loadOfficesFromHive();
     fetchOfficesFromSupabase();
-    _loadVersion();
   }
 
   void loadOfficesFromHive() {
@@ -66,108 +62,23 @@ class _HomePageState extends State<HomePage> {
     await fetchOfficesFromSupabase();
   }
 
-  Future<void> _loadVersion() async {
-    final info = await PackageInfo.fromPlatform();
-    setState(() {
-      _version = 'Version ${info.version}+${info.buildNumber}';
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.grey[100],
       appBar: AppBar(
-        backgroundColor: Colors.white,
-        elevation: 0,
-        iconTheme: const IconThemeData(color: Colors.black),
-        title: const Text(
-          'Offices',
-          style: TextStyle(color: Colors.black),
-        ),
-      ),
-      drawer: Drawer(
+        title: const Text('Pasacao Hotline'),
         backgroundColor: Colors.grey[100],
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Column(
-              children: [
-                // logo
-                DrawerHeader(
-                  child: Image.asset(
-                    'assets/images/logos/mayors_office.png',
-                    width: 500,
-                    // color: Colors.black,
-                  ),
-                ),
-
-                // Padding(
-                //   padding: const EdgeInsets.symmetric(horizontal: 25.0),
-                //   child: Divider(
-                //     color: Colors.grey[800],
-                //   ),
-                // ),
-
-                // other pages
-                Padding(
-                  padding: const EdgeInsets.only(left: 25.0),
-                  child: ListTile(
-                    leading: const Icon(
-                      Icons.feedback_outlined,
-                      color: Colors.black,
-                    ),
-                    title: const Text(
-                      'F E E D B A C K',
-                      style: TextStyle(color: Colors.black),
-                    ),
-                    onTap: () {
-                      Navigator.pop(context); // close drawer
-                      // You’ll hook up the Supabase feedback logic here later
-                      // showDialog(
-                      //   context: context,
-                      //   builder: (_) => const FeedbackDialog(),
-                      // );
-                      showFeedbackDialog(context);
-                    },
-                  ),
-                ),
-
-                Padding(
-                  padding: const EdgeInsets.only(left: 25.0),
-                  child: ListTile(
-                    leading: const Icon(
-                      Icons.info_outline,
-                      color: Colors.black,
-                    ),
-                    title: const Text(
-                      'A B O U T',
-                      style: TextStyle(color: Colors.black),
-                    ),
-                    onTap: () {
-                      Navigator.pop(context);
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (context) => const AboutPage()),
-                      );
-                    },
-                  ),
-                ),
-              ],
-            ),
-            Padding(
-              padding: const EdgeInsets.only(left: 25.0, bottom: 25.0),
-              child: ListTile(
-                title: Text(
-                  _version,
-                  style: const TextStyle(color: Colors.grey, fontSize: 14),
-                ),
-              ),
-            ),
-          ],
+        foregroundColor: Colors.black,
+        elevation: 0.5,
+        leading: Builder(
+          builder: (context) => IconButton(
+            icon: const Icon(Icons.menu),
+            onPressed: () => Scaffold.of(context).openDrawer(),
+          ),
         ),
       ),
+      drawer: const AppDrawer(),
       body: SafeArea(
         child: RefreshIndicator(
           color: Colors.grey[900],
@@ -181,13 +92,13 @@ class _HomePageState extends State<HomePage> {
                 child: Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    // const Text(
-                    //   'Offices',
-                    //   style: TextStyle(
-                    //     fontSize: 22,
-                    //     fontWeight: FontWeight.bold,
-                    //   ),
-                    // ),
+                    const Text(
+                      'Offices',
+                      style: TextStyle(
+                        fontSize: 22,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
                     const SizedBox(height: 12),
                     GridView.builder(
                       shrinkWrap: true,
